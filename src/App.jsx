b1a893186aa5b2061e8a63bfe01497530199542c
giuzep89 <g.rinaldimusic@gmail.com>
2025-11-05 13:16:06 +0100

    First 20 pokemon on the page, basic styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,68 @@
 import './App.css'
 import {useEffect, useState} from "react";
 import axios from "axios";
+import PokemonCard from "./components/pokemon-card/PokemonCard.jsx";
 
 function App() {
-    const [pokemonData, setPokemonData] = useState({});
     const [error, toggleError] = useState(false);
-    const {
-        name= "",
-        sprites: {front_default: picture = ""} = {},
-        moves= [],
-        weight= 0,
-        abilities= [],
-    } = pokemonData;
+    const [listOfPokemonUrls, setlistOfPokemonUrls] = useState([]);
+    const [listOfPokemonData, setlistOfPokemonData] = useState([]);
 
     useEffect(() => {
-        async function loadPokemon() {
+        async function loadListOfPokemon() {
             toggleError(false);
             try {
-                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/4/');
+                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0');
                 console.log(response);
-                setPokemonData(response.data);
+                setlistOfPokemonUrls(response.data.results);
             } catch (e) {
                 console.error(e);
                 toggleError(true);
             }
         }
-        loadPokemon();
-    }, []);
+        loadListOfPokemon();
+    }, [])
 
-  return (
-    <>
-      <h1>Gotta catch em all!</h1>
-        <div className="inner-container">
-        {Object.keys(pokemonData).length > 0 &&
-            <article className="pokemon-card">
-                <h2>{name}</h2>
-                <img src={picture} alt=""/>
-                <p>Moves: {moves.length}</p>
-                <p>Weight: {weight}</p>
-                <p>Abilities:</p>
-                {abilities.map((ability) => {
-                    return (
-                        <p key={ability.ability.name}>{ability.ability.name}</p>
-                    )
-                })}
-            </article>
+    useEffect(() => {
+        toggleError(false);
+        async function getPokemonDetails() {
+            try {
+                const result = listOfPokemonUrls.map((pokemon) => {
+                    return axios.get(pokemon.url);
+                })
+                const responses = await Promise.all(result);
+                const allData = responses.map((response) => {
+                    return response.data;
+                })
+                // console.log(allData[0]);
+                setlistOfPokemonData(allData);
+            } catch (e) {
+                console.error(e);
+                toggleError(true);
+            }
         }
-        </div>
-    </>
-  )
+
+        if (listOfPokemonUrls.length > 0) {
+            getPokemonDetails();
+        }
+    }, [listOfPokemonUrls]);
+
+
+    return (
+        <>
+            <h1>Gotta catch em all!</h1>
+            <div className="inner-container">
+                {listOfPokemonData.length > 0 &&
+                    listOfPokemonData.map(pokemon => {
+                        const {id, name, sprites: {front_default: picture}, moves, weight, abilities} = pokemon;
+                        return (
+                            <PokemonCard key={id} name={name} picture={picture} moves={moves} weight={weight} abilities={abilities} />
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
 }
 
 export default App
-
-// TODO - Make a pokemon card and then call it here to check if it works
-
-
---
+++ src/components/pokemon-card/PokemonCard.jsx
@@ -0,0 +1,18 @@
+import "./PokemonCard.css"
+
+export default function PokemonCard({name, picture, moves, weight, abilities}) {
+    return (
+    <article className="pokemon-card">
+        <h2>{name}</h2>
+        <img src={picture} alt=""/>
+        <p>Moves: {moves.length}</p>
+        <p>Weight: {weight}</p>
+        <p>Abilities:</p>
+        {abilities.map((ability) => {
+            return (
+                <p key={ability.ability.name}>{ability.ability.name}</p>
+            )
+        })}
+    </article>
+    )
+}
